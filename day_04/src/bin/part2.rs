a4a79d61072e06463f6e3541708769bce35cec72
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-12-04 22:04:52 +0000

    chore: Tidy up

--- day_04/src/bin/part2.rs
+++ day_04/src/bin/part2.rs
@@ -12,18 +12,14 @@ fn part2(input: &str) -> i32 {
       let mut match_count = 0;
       let card = &cards[i];
       // Count number of matching results
-      for num in result {
-        if card.contains(num) { match_count += 1; };
-      }
-
-      let card_multiplier = won_cards[i];
+      result.iter().filter(|num| card.contains(num)).for_each(|_| match_count += 1);
 
       // Count number of winning cards
       // Each matching result wins an additional subsequent card
       for j in 0..match_count {
         let win_index = i + j + 1;
         if win_index <= results.len() {
-          won_cards[win_index] += 1 * card_multiplier;
+          won_cards[win_index] += won_cards[i];
         };
       };
 
@@ -40,7 +36,7 @@ fn parse_input(input: &str) -> (Vec<Vec<i32>>, Vec<Vec<i32>>) {
 
     for line in input.lines() {
       let line = line.split(":").nth(1).expect("Unable to strip prefix from line");
-      let parts: Vec<Vec<i32>> = line.split("|").map(|part| parse_card_and_result(part)).collect();
+      let parts: Vec<Vec<i32>> = line.split("|").map(parse_card_and_result).collect();
       results.push(parts[0].clone());
       cards.push(parts[1].clone());
     }
@@ -52,7 +48,7 @@ fn parse_card_and_result(input: &str) -> Vec<i32> {
   input
     .trim()
     .split_whitespace()
-    .map(|r| r.parse::<i32>().expect("Unable to parse to number"))
+    .filter_map(|s| s.parse().ok())
     .collect()
 }
 
